375cd76b19be9b65ac353bda2bfe0a57a8c735ac
davidgithubby <31918922+davidgithubby@users.noreply.github.com>
2017-10-04 10:53:16 +0200

    david

--- Program.cs
+++ Program.cs
@@ -13,15 +13,12 @@ namespace testrepo
             //onze repository test
             Console.WriteLine("Laten we beginnen!!!");
             //verandering door stefan
-<<<<<<< HEAD
 
             Console.WriteLine("Test hallo! David hier.");
             Console.ReadLine();
 
             // test comment
-=======
             Console.WriteLine("Mark");
->>>>>>> 90dd38b7028df01bdf893eaabff82b2ce09783c2
             Console.WriteLine("Try");
 
             Console.WriteLine("lorem ipsum");

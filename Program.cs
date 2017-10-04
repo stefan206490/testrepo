90dd38b7028df01bdf893eaabff82b2ce09783c2
Márk Rendes <mark.rendes205727@student.leeuwenborgh.nl>
2017-10-04 10:38:03 +0200

    Me

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace testrepo
             //onze repository test
             Console.WriteLine("Laten we beginnen!!!");
             //verandering door stefan
+            Console.WriteLine("Mark");
         }
     }
 }
